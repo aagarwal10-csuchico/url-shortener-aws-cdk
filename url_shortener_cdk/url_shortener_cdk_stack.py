737b1ba5cb606a5911bcd05d843e0e51d18a052f
Apresh Agarwal <aagarwal10@csuchico.edu>
2026-02-10 03:10:10 -0600

    feat: add DynamoDB tables for url mappings and counter

--- url_shortener_cdk/url_shortener_cdk_stack.py
+++ url_shortener_cdk/url_shortener_cdk_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -10,10 +10,19 @@ class UrlShortenerCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # DynamoDB Tables
+        mappings_table = dynamodb.Table(
+            self, "UrlMappings",
+            partition_key={"name": "short_code", "type": dynamodb.AttributeType.STRING},
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+            time_to_live_attribute="expiration_time",
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "UrlShortenerCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        counters_table = dynamodb.Table(
+            self, "Counters",
+            partition_key={"name": "counter_id", "type": dynamodb.AttributeType.STRING},
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
