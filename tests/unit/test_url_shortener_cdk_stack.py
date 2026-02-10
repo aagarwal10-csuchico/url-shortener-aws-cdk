ed0e58e0831b966cb1a078bdf4a5212eeaf75079
Apresh Agarwal <aagarwal10@csuchico.edu>
2026-02-10 03:15:35 -0600

    test: add basic unit tests for DynamoDB tables and Lambda count

--- tests/unit/test_url_shortener_cdk_stack.py
+++ tests/unit/test_url_shortener_cdk_stack.py
@@ -1,15 +1,15 @@
-import aws_cdk as core
-import aws_cdk.assertions as assertions
+from aws_cdk import App
+from aws_cdk.assertions import Template
 
 from url_shortener_cdk.url_shortener_cdk_stack import UrlShortenerCdkStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in url_shortener_cdk/url_shortener_cdk_stack.py
-def test_sqs_queue_created():
-    app = core.App()
+def test_stack():
+    app = App()
     stack = UrlShortenerCdkStack(app, "url-shortener-cdk")
-    template = assertions.Template.from_stack(stack)
-
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    template = Template.from_stack(stack)
+    template.has_resource_properties("AWS::DynamoDB::Table", {
+        "BillingMode": "PAY_PER_REQUEST"
+    })
+    template.resource_count_is("AWS::Lambda::Function", 2)
